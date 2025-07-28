475657c087f80d843a97fa7825dbbf82186c318f
Ahmed Tamer Sameer <ahmedssworkgit@gmail.com>
2025-07-29 00:18:28 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 (function($, document, window){
 	
 	$(document).ready(function(){
-
-		// Cloning main navigation for mobile menu
+		
 		$(".mobile-navigation").append($(".main-navigation .menu").clone());
 
-		// Mobile menu toggle 
+		// Mobile menu toggle
 		$(".menu-toggle").click(function(){
 			$(".mobile-navigation").slideToggle();
 		});
